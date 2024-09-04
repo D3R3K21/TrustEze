905aa65716d8a4c53a56fc6968f93fec601bd088
Derek <lucidsolutionspro@gmail.com>
2024-09-04 13:48:35 -0700

    updated callback URI

--- trusteze-ui/src/components/Login.js
+++ trusteze-ui/src/components/Login.js
@@ -2,8 +2,8 @@ function Login() {
 
   //TODO:(drose) move URL to environment variable
     return (
-      <a href="https://trusteze.auth.us-east-1.amazoncognito.com/login?client_id=3gn8kb63bta2jmkjhcnhtnb4q0&response_type=code&scope=email+openid+phone&redirect_uri=http%3A%2F%2Flocalhost%3A80%2Fauth">
-        <button>Login</button>
+      <a href="https://trusteze.auth.us-east-1.amazoncognito.com/login?client_id=3gn8kb63bta2jmkjhcnhtnb4q0&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fapi.trusteze.co%2Fauth">
+        <button>Login - Styled By Derek with 0 css just for Britni</button>
       </a>
       );
 }
